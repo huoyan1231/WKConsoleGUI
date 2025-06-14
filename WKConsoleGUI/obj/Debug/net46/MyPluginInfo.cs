242c23288fc6ae7ea1b35a2d47912123e50472ef
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2025-06-14 18:33:02 +0800

    移除了编译出来的文件还有什么我不知道是啥的东西

--- WKConsoleGUI/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace WKConsoleGUI
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "WKConsoleGUI";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}

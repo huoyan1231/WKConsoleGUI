eac8cc86c50c4e64495a785dab56be208e2b6bd5
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2025-06-14 16:25:16 +0800

    我去，能用了
    只能F7打开控制台，先留个备份。

---
+++ Wkmod_1/obj/Debug/net46/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Wkmod_1
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Wkmod_1";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
